01b3192a84b51bed24640c12e8c88c1bfd2a9e97
Vachounet <vachounet@live.fr>
2020-01-31 15:26:32 +0100

    def: copy light hal to vendor_overlay

--- lights/Android.mk
+++ lights/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.light@2.0-service.def
 LOCAL_MODULE_TAGS  := optional
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/bin
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/29/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_STEM := android.hardware.light@2.0-service
 
@@ -34,7 +34,7 @@ LOCAL_MODULE := android.hardware.light@2.0-service.def.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/etc/init
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/29/etc/init
 LOCAL_MODULE_STEM := android.hardware.light@2.0-service.rc
 
 LOCAL_SRC_FILES := android.hardware.light@2.0-service.def.rc

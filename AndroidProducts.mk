d4cedb3778c5d8b1c9f144f5050bf3a38e25aabb
ph4n70m-404 <87523412+ph4n70m-404@users.noreply.github.com>
2021-11-16 18:49:26 -0600

    Change to spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/ROM_sofiar.mk
+    $(LOCAL_DIR)/spark_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    ROM_sofiar-user \
-    ROM_sofiar-userdebug \
-    ROM_sofiar-eng \
+    spark_sofiar-user \
+    spark_sofiar-userdebug \
+    spark_sofiar-eng \

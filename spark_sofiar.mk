c0e47ddfa4f57f0423992afc5a666e8b99e4540b
ph4n70m-404 <87523412+ph4n70m-404@users.noreply.github.com>
2021-11-22 18:38:00 -0600

    specify boot animation size to remove warnings

--- spark_sofiar.mk
+++ spark_sofiar.mk
@@ -34,6 +34,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common Spark stuff
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_BUILD_PRODUCT_IMAGE  := true

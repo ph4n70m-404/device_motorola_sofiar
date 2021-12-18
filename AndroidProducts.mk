fe0002f38409c4184686f0a8a297d0bd2a6ce2da
ph4n70m-404 <Ph4n70m-Github@protonmail.com>
2021-12-18 16:47:23 -0600

    change to lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/ROM_sofiar.mk
+    $(LOCAL_DIR)/lineage_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    ROM_sofiar-user \
-    ROM_sofiar-userdebug \
-    ROM_sofiar-eng \
+    lineage_sofiar-user \
+    lineage_sofiar-userdebug \
+    lineage_sofiar-eng \
